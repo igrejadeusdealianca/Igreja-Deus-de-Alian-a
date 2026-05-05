ec16d10fe6a20a8fe386c9c7b71310b4c769eee8
igrejadeusdealianca <danielmota.200308@gmail.com>
2026-05-05 11:33:27 -0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "SUA_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_PROJETO",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+  });
+});
